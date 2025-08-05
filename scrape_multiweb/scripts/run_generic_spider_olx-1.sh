45b3146ca349af8e7f02e71faf148803e127f5af
Revandi2245 <revandi.faudiamar.tif422@polban.ac.id>
2025-08-05 10:34:16 +0700

    [FIX] menghilangkan beberapa output json untuk debug pada portal yang aman, kecuali yang masih bermasalah

--- scrape_multiweb/scripts/run_generic_spider_olx-1.sh
+++ scrape_multiweb/scripts/run_generic_spider_olx-1.sh
@@ -14,20 +14,20 @@ else
     exit 1
 fi
 
-scrapy crawl generic_spider -a spider_name=hargo -a path=fixtures/hargo_config.json -o output/hargo.json  &
-scrapy crawl generic_spider -a spider_name=hotnews -a path=fixtures/hotnews_config.json -o output/hotnews.json &
+scrapy crawl generic_spider -a spider_name=hargo -a path=fixtures/hargo_config.json &
+scrapy crawl generic_spider -a spider_name=hotnews -a path=fixtures/hotnews_config.json &
 scrapy crawl generic_spider -a spider_name=jakartaglobe -a path=fixtures/jakartaglobe_config.json -o output/jakartaglobe.json &
-scrapy crawl generic_spider -a spider_name=karyaredaksi -a path=fixtures/karyaredaksi_config.json -o output/karyaredaksi.json &
-scrapy crawl generic_spider -a spider_name=marketing -a path=fixtures/marketing_config.json -o output/marketing.json &
-scrapy crawl generic_spider -a spider_name=pelitabatak -a path=fixtures/pelitabatak_config.json -o output/pelitabatak.json &
-scrapy crawl generic_spider -a spider_name=otobisnis -a path=fixtures/otobisnis_config.json -o output/otobisnis.json &
-scrapy crawl generic_spider -a spider_name=rajabalapotomotif -a path=fixtures/rajabalapotomotif_config.json -o output/rajabalapotomotif.json &
-scrapy crawl generic_spider -a spider_name=wartapol -a path=fixtures/wartapol_config.json -o output/wartapol.json &
-scrapy crawl generic_spider -a spider_name=vritta -a path=fixtures/vritta_config.json -o output/vritta.json &
+scrapy crawl generic_spider -a spider_name=karyaredaksi -a path=fixtures/karyaredaksi_config.json &
+scrapy crawl generic_spider -a spider_name=marketing -a path=fixtures/marketing_config.json &
+scrapy crawl generic_spider -a spider_name=pelitabatak -a path=fixtures/pelitabatak_config.json &
+scrapy crawl generic_spider -a spider_name=otobisnis -a path=fixtures/otobisnis_config.json &
+scrapy crawl generic_spider -a spider_name=rajabalapotomotif -a path=fixtures/rajabalapotomotif_config.json &
+scrapy crawl generic_spider -a spider_name=wartapol -a path=fixtures/wartapol_config.json &
+scrapy crawl generic_spider -a spider_name=vritta -a path=fixtures/vritta_config.json &
 scrapy crawl generic_spider -a spider_name=allrelease -a path=fixtures/allrelease_config.json -o output/allrelease.json &
-scrapy crawl generic_spider -a spider_name=bisnistrade -a path=fixtures/bisnistrade_config.json -o output/bisnistrade.json &
-scrapy crawl generic_spider -a spider_name=pojokindonesia -a path=fixtures/pojokindonesia_config.json -o output/pojokindonesia.json &
+scrapy crawl generic_spider -a spider_name=bisnistrade -a path=fixtures/bisnistrade_config.json &
+scrapy crawl generic_spider -a spider_name=pojokindonesia -a path=fixtures/pojokindonesia_config.json &
 scrapy crawl generic_spider -a spider_name=selular -a path=fixtures/selular_config.json -o output/selular.json &
-scrapy crawl generic_spider -a spider_name=sonaindonesia -a path=fixtures/sonaindonesia_config.json -o output/sonaindonesia.json &
+scrapy crawl generic_spider -a spider_name=sonaindonesia -a path=fixtures/sonaindonesia_config.json &
 
 wait
